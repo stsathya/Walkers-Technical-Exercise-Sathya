24504ca521493b7bc92c6ac35b632d990d8604a5
Sathya Seelan <saseel.tech@outlook.com>
2021-11-19 14:26:09 +0000

    app change

---
+++ NewApiapp/Assignment.cs
@@ -0,0 +1,11 @@
+namespace NewApiapp
+{
+    public class Assignment
+    {
+        public DateTime Date { get; set; }
+
+        public int Number { get; set; }
+
+        public string? Title { get; set; }
+    }
+}
